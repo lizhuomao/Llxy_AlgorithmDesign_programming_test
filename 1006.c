9fc3b8e22d33093b4b91c8ec544e2dbea8fc5ec8
DanDan12345678 <38347981+DanDan12345678@users.noreply.github.com>
2018-04-17 20:53:26 +0800

    Add files via upload

---
+++ 1006.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    char a,da,b,bd;
+    int i=1,g=1,pa,pb,zf;
+    a=getchar();
+    da=getchar();
+    b=getchar();
+    bd=getchar();
+    while('a'<='\n')
+    {
+        if('a'=='da')
+            i++;
+    }
+    while('b'<='bd')
+    {
+        if('b'=='bd')
+            g++;
+    }
+    pa=i*da;
+    pb=g*bd;
+    zf=pa+pb;
+    printf("%d",zf);
+    return 0;
+}
